96369be69012a943bf50d12561aef6a7a7f356f7
Pablo Saraiva <pablosaraiva@gmail.com>
2015-06-21 21:39:37 -0300

    Some Scripts

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+
+	public Room nextRoom;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Sinner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sinner : MonoBehaviour {
+
+	public Room currentRoom;
+	private float timeInRoom = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeInRoom = timeInRoom + Time.deltaTime;
+		if (timeInRoom > 1) {
+			timeInRoom = 0;
+			GoToRoom(currentRoom.nextRoom);
+		}
+
+	}
+
+	private void GoToRoom(Room nextRoom) {
+		if (nextRoom != null) {
+			currentRoom = nextRoom;
+			gameObject.transform.position = nextRoom.transform.position;
+		} else {
+			Destroy(gameObject);
+		}
+	}
+
+}
