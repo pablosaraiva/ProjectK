a4306dbc4fae21472fe2503c7b329c7a42faab40
Pablo Saraiva <pablosaraiva@gmail.com>
2015-07-05 12:26:18 -0300

    Room placement aligned to grid

--- Assets/Scripts/UIScripts/NewPunishmentRoomButton.cs
+++ Assets/Scripts/UIScripts/NewPunishmentRoomButton.cs
@@ -5,6 +5,7 @@ public class NewPunishmentRoomButton : MonoBehaviour {
 
 	public GameObject punishmentRoomPrefab;
 	private GameObject newRoom;
+	public Vector2 gridSize = new Vector2 (140, 70);
 
 	void Start() {
 		CanvasRenderer renderer = GetComponent<CanvasRenderer> () as CanvasRenderer;
@@ -17,8 +18,15 @@ public class NewPunishmentRoomButton : MonoBehaviour {
 
 	public void OnGUI() {
 		if (newRoom != null) {
-			Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			newRoom.transform.position =  position;
+			Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			int gridPositionX = Mathf.RoundToInt (mouseWorldPosition.x / gridSize.x);
+			int gridPositionY = Mathf.RoundToInt(mouseWorldPosition.y / gridSize.y);
+			Vector2 worldPosition = new Vector2(gridSize.x * gridPositionX, gridSize.y * gridPositionY);
+			print ("Mouse Position: " + Input.mousePosition);
+			print ("Mouse World Position: " + mouseWorldPosition);
+			print ("Grid Position: " + gridPositionX + ", " + gridPositionY);
+			print ("World Position: " + worldPosition);
+			newRoom.transform.position =  worldPosition;
 			if (Input.GetButtonDown("Fire1")) {
 				newRoom = null;
 			}
