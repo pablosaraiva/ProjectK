382764494dab3377793a6688e9a061d07d7b7a2f
StefanW <scaw.dev@gmail.com>
2015-07-17 03:34:10 +0200

    Pipe Arrow Animation

---
+++ Assets/Scripts/PipeArrow.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PipeArrow : MonoBehaviour {
+
+	private float speed = 20f;
+
+	private Coroutine mouvementCoroutine = null;
+
+	private SpriteRenderer spriteRenderer;
+
+	public void Awake(){
+		spriteRenderer = this.GetComponent<SpriteRenderer> ();
+	}
+
+	public void MoveToTarget(Vector3 target, Action<PipeArrow> onMoveFinishCallback){
+		if (mouvementCoroutine != null) {
+			StopCoroutine (mouvementCoroutine);
+		}
+		mouvementCoroutine = StartCoroutine(Movement(target, onMoveFinishCallback));
+	}
+
+	
+	IEnumerator Movement (Vector3 target, Action<PipeArrow> onMoveFinishCallback) {
+
+		while (Vector3.Distance(transform.position, target) > 0.05f) {
+			transform.position = Vector3.MoveTowards(transform.position, target, speed*Time.deltaTime);
+			yield return null;
+		}
+
+		onMoveFinishCallback (this);
+	}
+
+	public void SetHighLight(bool highlight){
+		//print ("High");
+		Color c = spriteRenderer.color;
+		if (highlight) {
+			speed = 60f;
+			c.a = 200F/255F;
+		}else{
+			speed = 20f;
+			c.a = 60F/255F;
+		}
+		spriteRenderer.color = c;
+	}
+}
--- Assets/Scripts/RoomScripts/PipeScript.cs
+++ Assets/Scripts/RoomScripts/PipeScript.cs
@@ -1,13 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PipeScript : MonoBehaviour {
 
 	private bool highlight = false;
 
 	private SpriteRenderer pipeRenderer;
+
+	private Vector3 enterPoint, exitPoint;
+
+	private List<PipeArrow> pipeArrowsList = new List<PipeArrow> ();
+
+	public GameObject pipeArrowPrefab;
+
 	void Start(){
 		pipeRenderer = transform.Find ("PipeImage").GetComponent<SpriteRenderer> ();
+
 	}
 
 	public bool HighLight {
@@ -23,14 +32,18 @@ public class PipeScript : MonoBehaviour {
 				c.a = 45F/255F;
 			}
 			pipeRenderer.color = c;
+
+			foreach(PipeArrow arrow in pipeArrowsList){
+				arrow.SetHighLight(highlight);
+			}
 		}
 	}
 
 	//TODO link on a better way, for no total overlapsing
 	//TODO Clean this mess
 	public void SetPositionAndScale(Transform roomFrom, Transform roomTo){
-		Vector3 enterPoint = roomFrom.position + new Vector3 (Room.roomWidth / 2F, Room.roomHeight / 2F, 0);
-		Vector3 exitPoint = roomTo.position + new Vector3 (- Room.roomWidth / 2F, Room.roomHeight / 2F, 0);
+		enterPoint = roomFrom.position + new Vector3 (Room.roomWidth / 2F, Room.roomHeight / 2F, 0);
+		exitPoint = roomTo.position + new Vector3 (- Room.roomWidth / 2F, Room.roomHeight / 2F, 0);
 		//Debug.Log (roomTo.position);
 		transform.position = ((exitPoint + enterPoint) / 2F);
 		transform.rotation = Quaternion.identity;
@@ -40,5 +53,30 @@ public class PipeScript : MonoBehaviour {
 			angle = 360 - angle;
 		transform.Rotate (new Vector3 (0,0,angle));
 		transform.localScale = new Vector3 (Vector3.Distance(enterPoint, exitPoint), 1, 1);
+
+		ClearArrows ();
+		CancelInvoke ();
+		InvokeRepeating ("CreateArrows", 0.8f, 0.8f);
 	}
+
+	public void CreateArrows(){
+		PipeArrow arrow = (Instantiate (pipeArrowPrefab, enterPoint, this.transform.rotation) as GameObject).GetComponent<PipeArrow>();
+		pipeArrowsList.Add (arrow);
+		arrow.SetHighLight (highlight);
+		arrow.MoveToTarget (exitPoint, PipeArrowArriveEndCallback);
+	}
+
+	public void PipeArrowArriveEndCallback(PipeArrow arrow){
+		pipeArrowsList.Remove (arrow);
+		Destroy (arrow.gameObject);
+	}
+
+	private void ClearArrows(){
+		foreach (PipeArrow arrow in pipeArrowsList) {
+			Destroy(arrow.gameObject);
+		}
+		pipeArrowsList.Clear ();
+	}
+
+
 }
